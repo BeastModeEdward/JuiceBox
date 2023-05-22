2eba2c52d340a832f0fa8fe6c174635528a939bb
Edward <puffcorn1213@gmail.com>
2023-05-21 17:56:11 -0700

    part 2 done

--- db/index.js
+++ db/index.js
@@ -297,6 +297,19 @@ async function createPostTag(postId, tagId) {
       throw error;
     }
   } 
+  async function getUserByUsername(username) {
+    try {
+      const { rows: [user] } = await client.query(`
+        SELECT *
+        FROM users
+        WHERE username=$1;
+      `, [username]);
+  
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
 
 module.exports = {  
   client,
@@ -312,5 +325,6 @@ module.exports = {
   createPostTag,
   getPostById,
   addTagsToPost,
-  getPostsByTagName
+  getPostsByTagName,
+  getUserByUsername
 }
